784309e3b34c18521481c162c2124045db61be08
Muneer <muneer509@gmail.com>
2024-08-27 00:16:41 +0530

    updated

--- sum.sh
+++ sum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 number1=$1
 number2=$2
-SUM=$(($number1+$number2))
-echo "sum of $number1 and $number2 is: $SUM"
+
+add=$(($number1+$number2))
+echo "sum of $number1 and $number2 is: $add"
